d9ddcaf33ef6ec3616dd838a651b0d53d30ccd76
João Paulo Rocha <jporocha@gmail.com>
2021-10-20 13:54:15 -0300

    agora com o dist

---
+++ frontend/dist/js/chunk-2d0da3ed.b46ee684.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0da3ed"],{"6b7b":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"schedule"},[t("h1",[e._v("Página para cadastro de consultas")])])}],c=t("2877"),l={},r=Object(c["a"])(l,a,s,!1,null,null,null);n["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d0da3ed.b46ee684.js.map
