f23bffc960e779dd7b8028274a9a2cf0644a8dfc
jporocha <46237098+jporocha@users.noreply.github.com>
2022-10-25 12:36:13 -0300

    Update mongoose.js

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 
-const connectionString = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@tcc.uzrpa.mongodb.net/${process.env.MONGO_BASE}`;
+const connectionString = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.gnpzopc.mongodb.net/${process.env.MONGO_BASE}`;
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
 });
 
 const db = mongoose.connection;
